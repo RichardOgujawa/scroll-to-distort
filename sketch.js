6caef3bcb08dbc26fe514c7f2a089d9d5b1619f8
Richard Ogujawa <richardogujawa7@gmail.com>
2022-03-19 11:04:29 +0000

    final tweaks

--- sketch.js
+++ sketch.js
@@ -1,63 +1,66 @@
 //IMAGE VARIABLES + EFFECTS VARIABLES
 let imgAdult;
-let imgBaby;
-let x1;
-let y1;
-let x2;
-let y2;
-let x3;
-let y3;
-let x4;
-let y4;
-let w;
-let h;
+let pixels = [];
 
 //TEXT VARIABLES
-let sentence;
+let sentence = [];
 let indicator;
 
 
 function preload() {
-  imgBaby = loadImage('data/images/1early-childhood.jpg');
   imgAdult = loadImage('data/images/7early-adulthood-sad.jpg')
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(750, windowHeight*0.97);
   image(imgAdult, 0, 0);
-  sentence = ["The", "more", "we", "look", "the", "less", "we", "see."]
+  sentence = ["The", "more", "you", "scroll", "the", "more", "distorted", "reality", "becomes."]
+  indicator = 0;
+  noLoop(); //only want to initiate loop on mouse wheel scroll, so will initiate it later
 }
 
 
 function draw() {
-  //DISTORTION FX
-  let x1Higher = floor(map(mouseY, height, 0, 0, 200));
-  x1 = random(0, x1Higher);
-  y1 = random(x1Higher, x1Higher + 100);
+    //TEXT ANIMATION
+    fill(255);
+    noStroke();
+    rectMode(CENTER);
+    rect(620, 400, 190, 100);
+    //Covering the previous text each time so they don't overlay
+  
+  
+    fill(0);
+    textSize(40);
+    textAlign(CENTER, CENTER)
+    indicator = floor(map(mouseY, 20, height, sentence.length - 1, 0));
+    loop()
+    text(sentence[indicator], 620, 400);
+    //cycle through the sentence array depending on the mouseY position
+}
 
-  x2 = round(x1 + random(-10, 10));
-  y2 = round(y1 + random(-10, 10));
-
-  w = 300;
-  h = 5;
-
-  if (y1 >= 230 && y1 <= 245) {
-    set(x2, y2, get(x1, y1, w, h));
+function mouseWheel() {
+  for (var i = 0; i < mouseY; i++) {
+    pixels[i] = new Pixel();
+    pixels[i].display();
   }
-  else {
-  }
+  //Create a new pixel every time mouse wheel is scrolled
+}
+function Pixel() {
+  this.x = random(0, width);
+  this.y = random(0, height);
 
-  //TEXT ANIMATION
-  fill(255);
-  noStroke();
-  rectMode(CORNER);
-  rect(windowWidth * 0.68, windowHeight * 0.52, 190, 100);
+  this.display = function () {
+    let c = color(imgAdult.get(this.x, this.y));
+    stroke(c);
+    noFill();
+    rect(this.x, this.y, 4, 6);
+  };
+  //Parameters/Properties for each new pixel created
+}
 
-  fill(0);
-  textSize(40);
-  textAlign(CENTER, CENTER)
-  indicator = floor(map(mouseY, 0, height, sentence.length - 1, 0));
-  text(sentence[indicator], windowWidth * 0.77, 400);
+function mousePressed() {
+  Loop();
 
 }
+
 
